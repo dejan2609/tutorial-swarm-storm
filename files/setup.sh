0bd1d0c30fbf35caf2172a559de15653ee4eadb7
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-21 12:32:56 +0100

    more automation

--- files/setup.sh
+++ files/setup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Setup ZooKeeper
+. files/setupZooKeeper.sh
+
+# Wait for a majority of the ZooKeeper ensemble before proceeding with installation
+MODE=
+while [ -z $MODE ]
+do
+    echo "waiting for ZooKeeper ensemble to become healthy..."
+    sleep 1
+    MODE=$(. files/zookeeperHealthcheck.sh | grep ^Mode: | awk '{print $2;}')
+done
+echo "ZooKeeper ensemble up and running! This node is $MODE"
+
+
 # the first server in the ZooKeeper server list is the Swarm managing node  --> all others are pure workers
 if [ $ZOOKEEPER_ID == "1" ]
 then
