51713aee3039981e6c470ff9686d55b79caa72b5
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-04-06 12:51:11 +0200

    test: change docker install script

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list.d/docker.list
-sudo apt-get update && sudo apt-get purge lxc-docker && sudo apt-cache policy docker-engine
-sudo apt-get update -y && sudo  apt-get install -y linux-image-extra-$(uname -r) apparmor docker-engine git make
-
-sudo usermod -aG docker $(whoami)
+apt-get update
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee -a /etc/apt/sources.list.d/docker.list
+apt-get update && apt-get purge lxc-docker && apt-cache policy docker-engine
+apt-get update -y &&  apt-get install -y linux-image-extra-$(uname -r) apparmor docker-engine git make
