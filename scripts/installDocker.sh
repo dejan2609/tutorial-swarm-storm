9863e3e639841062932cda339f0b5c2350805a4d
Lior Sabag <lior@trendiguru.com>
2016-10-13 01:05:17 +0300

    update to 16.04

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee -a /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
 apt-get update && apt-get purge lxc-docker && apt-cache policy docker-engine
 apt-get update -y &&  apt-get install -y linux-image-extra-$(uname -r) apparmor docker-engine git make
 
